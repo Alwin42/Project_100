792c81f0aba9885ced667c14afbc2730e90beff2
Alwin42 <alwinemmanuel424@gmail.com>
2025-06-01 23:52:09 +0530

    save Notes and local storage

--- DoNotes/Notes/script.js
+++ DoNotes/Notes/script.js
@@ -3,6 +3,18 @@ const createBtn = document.querySelector(".btn");
 
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes()
+{
+    notesContainer.innerHTML = localStorage.getItem("notes"); 
+}
+
+showNotes();
+
+function saveNotes()
+{
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 createBtn.addEventListener("click", () => {
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -17,5 +29,15 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
+        saveNotes();
+    }
+    else if (e.target.tagName === "P")
+        {
+        notes = document.querySelectorAll(".input-box"); 
+        notes.forEach(nt =>{
+            nt.onkeyup = function() {
+                saveNotes();
+            }
+        })
     }
 });
