1ff8c13c8a9a465445a475a3055dea58f9d40275
Alwin42 <alwinemmanuel424@gmail.com>
2025-06-26 23:53:44 +0530

    app route

--- Agro-Tech/app.py
+++ Agro-Tech/app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/crops')
+def crops():
+    return render_template('crops.html')
+
+@app.route('/marketplace')
+def marketplace():
+    return render_template('marketplace.html')
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
+
+@app.route('/login')
+def login():
+    return render_template('Login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
