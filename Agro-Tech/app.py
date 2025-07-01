ca36eb3613d43323bf4e3eb6591d20d1486a83c4
Alwin42 <alwinemmanuel424@gmail.com>
2025-07-01 23:42:38 +0530

    admin and user login feature added

--- Agro-Tech/app.py
+++ Agro-Tech/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request , redirect, url_for , session
 
 app = Flask(__name__)
 
@@ -14,13 +14,49 @@ def crops():
 def marketplace():
     return render_template('marketplace.html')
 
-@app.route('/weather')
-def weather():
-    return render_template('weather.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('Login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user=users.get(username)
+        if user and user['password'] == password:
+            return redirect(url_for('dashboard'))
+        else:
+            return "Invalid Username or Password",401
+    
+    return render_template('login.html')
+
+
+users= {
+    'Alwin':
+    {
+        'password': "1234",
+        'email': "alwin@gmail.com"
+    },
+
+    'Admin':
+    {
+        'password': "admin123",
+        'email': "admin123@gmail.com"
+    }
+}
+
+@app.route('/dashboard')
+def dashboard():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
+    username = session['username']
+    user = users[username]
+    return render_template('dashboard.html', username=username, email=user['email'])
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
