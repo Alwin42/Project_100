a9621326d85a31f3791983e87b6c8fd78a3758fe
Alwin42 <alwinemmanuel424@gmail.com>
2025-08-27 00:03:32 +0530

    about and contact page

--- Synapse/routes.py
+++ Synapse/routes.py
@@ -5,5 +5,13 @@ routes = Blueprint('routes', __name__)
 
 @routes.route('/service')
 def service():
-    
+
     return render_template('services.html')
+
+@routes.route('/about')
+def about():
+    return render_template('about.html')
+
+@routes.route('/contact')
+def contact():
+    return render_template('contact.html')
