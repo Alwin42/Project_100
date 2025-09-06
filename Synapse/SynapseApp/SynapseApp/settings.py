e2390e31654b77c6e4d4cb24ec8b6c0d3d00654d
Alwin42 <alwinemmanuel424@gmail.com>
2025-09-06 23:17:48 +0530

    templates and home page

--- Synapse/SynapseApp/SynapseApp/settings.py
+++ Synapse/SynapseApp/SynapseApp/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'SynapseApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
