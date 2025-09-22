1c7b2a8f70733a90d16a7598abf8677bfa88e41d
Alwin <alwinemmanuel424@gmail.com>
2025-09-22 20:12:55 +0530

    buttons and register link

--- Deadline-Dash/app.py
+++ Deadline-Dash/app.py
@@ -1,17 +1,24 @@
 
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dash.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
