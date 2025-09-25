346c42b0a6f0ef758261497075a8fea3702086d8
Alwin <alwinemmanuel424@gmail.com>
2025-09-25 23:48:03 +0530

    Service page UI done

--- Eco-Sync/App/urls.py
+++ Eco-Sync/App/urls.py
@@ -4,5 +4,7 @@ from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  
+    path('', views.home, name='home'), 
+    path('about/', views.about, name='about'), 
+    path('service/', views.service, name='service'),
 ]
--- Eco-Sync/main/views.py
+++ Eco-Sync/main/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'main.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def service(request):
+    return render(request, 'service.html')
