6bdb7eae12c5c57760efe832a0f06a619491b739
Alwin <alwinemmanuel424@gmail.com>
2025-10-08 23:14:20 +0530

    deleted databased models.py and rewiring

--- Pearl-Pavilion/app.py
+++ Pearl-Pavilion/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_login import login_required, current_user 
 
-# App Configuration
+# App Configuration 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotel.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = 'your_secret_key'
+
 
 
 @app.route('/')
@@ -19,6 +26,29 @@ def booking():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     return render_template('contact.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('home'))
+
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    
+    return render_template('dashboard.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():        
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
